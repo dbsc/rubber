dbsc/rubber

commit 52a36a2fdb02c5e6711d8b0070fc2e37c202c5ef

Fail gracefully if rubber.version could not be imported

rubber/cmdline.py
@@ -21,7 +21,12 @@ from rubber.util import _
 import logging
 msg = logging.getLogger (__name__)
 import rubber.util
-import rubber.version
+
+try:
+    import rubber.version
+    RUBBER_VERSION=rubber.version.version
+except ImportError:
+    RUBBER_VERSION="unknown"
 
 # The expected entry point is the main procedure, with one of these
 # three values to track the command name (which may differ from
@@ -193,7 +198,7 @@ def parse_opts (command_name):
         help='increase verbosity (may be repeated)')
 
     parser.add_argument ('--version', action='version',
-        version='%(prog)s ' + rubber.version.version)
+        version='%(prog)s ' + RUBBER_VERSION)
 
     warn_values = ('all', 'boxes', 'misc', 'refs')
     class WarnAction (argparse.Action):
@@ -276,7 +281,7 @@ def main (command_name):
     try:
         options = parse_opts (command_name)
 
-        msg.debug (_("This is Rubber version %s.") % rubber.version.version)
+        msg.debug(f"This is Rubber version ${RUBBER_VERSION}.")
 
         if command_name == RUBBER_PIPE:
             # Generate a temporary source file, and pretend it has

tests/run.sh
@@ -39,12 +39,10 @@ python=python3
 
 echo "When a test fails, please remove the $tmpdir directory manually."
 
-# Copy source directory, because we must patch version.py and python
+# Copy source directory, because python
 # will attempt to write precompiled *.pyc sources.  For efficiency,
 # we share these temporary files among tests.
 cp -a "$SOURCE_DIR/rubber" $tmpdir/rubber
-sed "s%@version@%unreleased%;s%@moddir@%$SOURCE_DIR/data%" \
-    $tmpdir/rubber/version.py.in > $tmpdir/rubber/version.py
 # Also rename rubber to rubber.py to avoid a clash with rubber/.
 for exe in rubber rubber-info rubber-pipe; do
     cp "$SOURCE_DIR/bin/$exe" $tmpdir/$exe.py

commit 3d8ec61a5521e34f14ac3b91ef8070adba81b94a

Re-add errornously removed _ gettext() idiom
I removed accidentially with 52a36a2 ("Fail gracefully if
rubber.version could not be imported") the _ call. Rubber uses the _
idiom, for a, not yet existing, gettext internationalization and
localization layer.

Fixes: 52a36a2 ("Fail gracefully if rubber.version could not be imported")

rubber/cmdline.py
@@ -281,7 +281,7 @@ def main (command_name):
     try:
         options = parse_opts (command_name)
 
-        msg.debug(f"This is Rubber version ${RUBBER_VERSION}.")
+        msg.debug(_("This is Rubber version %s.") % RUBBER_VERSION)
 
         if command_name == RUBBER_PIPE:
             # Generate a temporary source file, and pretend it has
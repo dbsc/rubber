dbsc/rubber

commit 82f745491f64eba167fe9fdbce07a12eccad3310

Add myself as maintainer

setup.py
@@ -213,6 +213,8 @@ Metapost compilation).\
 """,
     author = 'Sebastian Kapfer',
     author_email = 'sebastian.kapfer@fau.de',
+    maintainer = 'Florian Schmaus',
+    maintainer_email = 'flo@geekplace.eu',
     url = 'https://gitlab.com/latex-rubber/rubber',
     license = "GPL",
     packages = (

README
@@ -113,7 +113,7 @@ Rubber tries to err on the side of caution here.
 * Author
 
 Rubber was originally written by Emmanuel Beffara <emmanuel@beffara.org>.
-It is currently maintained by Sebastian Kapfer <sebastian.kapfer@fau.de>
+It is currently maintained by Florian Schmaus <flo@geekplace>, Sebastian Kapfer <sebastian.kapfer@fau.de>
 and Nicolas Boulenguez <nicolas@debian.org>.
 
 Its homepage can be found at https://gitlab.com/latex-rubber/rubber

doc/man-en/rubber-info.1.in
@@ -67,7 +67,7 @@ contain the string "Warning".
 There are surely a some...
 
 This page documents Rubber version @version@.
-The program and this man-page are maintained by @author@ <@author_email@>.
+The program and this man-page are maintained by @maintainer@ <@maintainer_email@>.
 The homepage for Rubber can be found at @url@.
 
 .SH SEE ALSO

doc/man-en/rubber.1.in
@@ -641,7 +641,7 @@ applies to.
 There are surely a some...
 .PP
 This page documents Rubber version @version@.
-The program and this man-page are maintained by @author@ <@author_email@>.
+The program and this man-page are maintained by @maintainer@ <@maintainer_email@>.
 The homepage for Rubber can be found at @url@.
 .
 .SH SEE ALSO

doc/man-fr/rubber-info.1.in
@@ -73,7 +73,7 @@ toutes les lignes du fichier log qui contiennent « Warning ».
 Il y en a surement quelques uns...
 
 Cette page se rapporte à la version @version@ de Rubber. Le programme et cette
-documentation sont maintenus par @author@ <@author_email@>.
+documentation sont maintenus par @maintainer@ <@maintainer_email@>.
 La page web du programme se trouve à l'adresse @url@.
 
 .SH VOIR AUSSI

doc/man-fr/rubber.1.in
@@ -662,7 +662,7 @@ commande.
 Il y en a surement quelques uns...
 .PP
 Cette page se rapporte à la version @version@ de Rubber. Le programme et cette
-documentation sont maintenus par @author@ <@author_email@>.
+documentation sont maintenus par @maintainer@ <@maintainer_email@>.
 La page web du programme se trouve à l’adresse @url@.
 .
 .SH VOIR AUSSI

commit 003a74938e32ab43f3607eb4fd14c411007db1ac

[setup] Add @maintainer@ and @maintainer_email@ to the list of substitutions
We changed the generated man pages to use @maintainer@ instead of
@author@ with 82f7454 ("Add myself as maintainer"), but I forgot to
add those strings to the list of substitutions in setup.py.

Fixes: 82f7454 ("Add myself as maintainer")

setup.py
@@ -90,7 +90,7 @@ class build (distutils.command.build.build):
 
     def run (self):
         subs = {}
-        for v in ("author", "author_email", "url", "version"):
+        for v in ("author", "author_email", "maintainer", "maintainer_email", "url", "version"):
             subs [v] = getattr (self.distribution.metadata, "get_"+v) ()
         self.generate_files_with_substitutions (subs)
 